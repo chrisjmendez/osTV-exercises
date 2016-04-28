c653bffbaf69dc949eab97b22cd1b098b318ebec
Chris Mendez <cmendez@silverbullet.local>
2016-04-28 07:26:08 -0700

    Added movies demo

---
+++ Movies/Movies/MovieCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieCollectionViewCell.swift
+//  Movies
+//
+//  Created by Chris Mendez on 4/21/16.
+//  Copyright © 2016 Chris Mendez. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var movieImage:UIImageView!
+    @IBOutlet weak var movieLabel:UILabel!
+    
+    func configureCell(){
+        
+    }
+}
